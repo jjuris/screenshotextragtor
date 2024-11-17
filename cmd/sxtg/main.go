13a2d4a312105b664d74d363b54f7d91586f95ec
Jozef Juris <jjuris@gmail.com>
2024-11-17 19:46:09 +0100

    Add debug flag to screenshot extractor for enhanced logging

--- cmd/sxtg/main.go
+++ cmd/sxtg/main.go
@@ -15,8 +15,9 @@ func main() {
 	outputDir := flag.String("o", ".", "Output directory")
 	count := flag.Int("c", 1, "Count of screenshots")
 	interval := flag.Int("t", 1, "Time interval between screenshots in seconds")
+	debug := flag.Bool("d", false, "Enable debug messages")
 	flag.Usage = func() {
-		fmt.Println("Usage: sxtg -i <video_file> [-o output_directory] [-c count] [-t interval]")
+		fmt.Println("Usage: sxtg -i <video_file> [-o output_directory] [-c count] [-t interval] [-d]")
 		flag.PrintDefaults()
 	}
 	flag.Parse()
@@ -34,24 +35,28 @@ func main() {
 	}
 
 	// Print the parsed arguments (for debugging purposes)
-	fmt.Printf("Video File: %s\n", *inputFile)
-	fmt.Printf("Output Directory: %s\n", *outputDir)
-	fmt.Printf("Count: %d\n", *count)
-	fmt.Printf("Interval: %d\n", *interval)
+	if *debug {
+		fmt.Printf("Video File: %s\n", *inputFile)
+		fmt.Printf("Output Directory: %s\n", *outputDir)
+		fmt.Printf("Count: %d\n", *count)
+		fmt.Printf("Interval: %d\n", *interval)
+	}
 
 	// Extract screenshots
 	for i := 0; i < *count; i++ {
 		timestamp := i * *interval
 		outputFile := filepath.Join(*outputDir, fmt.Sprintf("%s_%d.png", filepath.Base(*inputFile), i+1))
-		err := extractScreenshot(*inputFile, outputFile, timestamp)
+		err := extractScreenshot(*inputFile, outputFile, timestamp, *debug)
 		if err != nil {
 			fmt.Printf("Error extracting screenshot at %d seconds: %v\n", timestamp, err)
 		}
 	}
 }
 
-func extractScreenshot(videoFile, outputFile string, timestamp int) error {
-	fmt.Printf("Timestamp: %s\n", strconv.Itoa(timestamp))
+func extractScreenshot(videoFile, outputFile string, timestamp int, debug bool) error {
+	if debug {
+		fmt.Printf("Extracting screenshot at %d seconds\n", timestamp)
+	}
 	cmd := exec.Command("ffmpeg", "-ss", strconv.Itoa(timestamp), "-i", videoFile, "-vframes", "1", outputFile)
 	return cmd.Run()
 }
